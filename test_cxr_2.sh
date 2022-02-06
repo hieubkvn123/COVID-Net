ed0f1ba6273bd096ade8f1fa5b9e594a0c3a1fb3
hieubkvn123 <hieubkvn123@gmail.com>
2022-02-06 11:00:58 +0700

    Finalised testing files

--- test_cxr_2.sh
+++ test_cxr_2.sh
@@ -1,9 +1,9 @@
-python3 inference.py \                                                                                                 minhhieu@minhhieu-Z490-GAMING-X 10:52:24
+python3 inference.py \
     --weightspath models/COVIDNet-CXR-2 \
     --metaname model.meta \
     --ckptname model \
     --n_classes 2 \
     --imagepath assets/ex-covid.jpeg \
-    --in_tensorname input_1:0 \
-    --out_tensorname norm_dense_2/Softmax:0
+    --out_tensorname norm_dense_2/Softmax:0 \
+	--in_tensorname input_1:0 \
 
